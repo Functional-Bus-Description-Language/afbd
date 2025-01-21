7a80c99c34665b8aef0ed168c861b3535fc4820b
Michał Kruszewski <mkru@protonmail.com>
2025-01-21 16:44:58 +0100

    Bring back config-5 tb

---
+++ tests/co-simulations/config/5/vhdl-apb/tb.vhd
@@ -0,0 +1,43 @@
+library work;
+  context work.cosim_context;
+  use work.cosim.all;
+
+library ltypes;
+  use ltypes.types.all;
+
+entity tb_cosim is
+  generic(
+    G_SW_GW_FIFO_PATH : string;
+    G_GW_SW_FIFO_PATH : string
+  );
+end entity;
+
+
+architecture test of tb_cosim is
+
+  signal clk : std_logic := '0';
+
+  signal cfgs : slv_vector(9 downto 0)(16 downto 0);
+
+  signal req : requester_out_t;
+  signal com : completer_out_t;
+
+begin
+
+  clk <= not clk after 0.5 ns;
+
+
+  cosim_interface(G_SW_GW_FIFO_PATH, G_GW_SW_FIFO_PATH, clk, req, com);
+
+
+  afbd_main : entity afbd.Main
+  port map (
+    clk_i => clk,
+    rst_i => '0',
+    reqs_i(0) => req,
+    reqs_o(0) => com,
+    Cfgs_o => cfgs,
+    Sts_i  => cfgs
+  );
+
+end architecture;
