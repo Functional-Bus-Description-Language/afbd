5c5cfaaccfb9a7c2bdbc624f285e0dba01519e8d
Michał Kruszewski <mkru@protonmail.com>
2025-01-21 13:39:48 +0100

    Update vhdl-amba5 submodule

--- tests/co-simulations/common/vhdl-apb/cosim/src/cosim_context.vhd
+++ tests/co-simulations/common/vhdl-apb/cosim/src/cosim_context.vhd
@@ -1,21 +1,16 @@
 context cosim_context is
-   library ieee;
-      use ieee.std_logic_1164.all;
-      use ieee.numeric_std.all;
+  library ieee;
+    use ieee.std_logic_1164.all;
+    use ieee.numeric_std.all;
 
-   library afbd;
-      use afbd.apb;
-      use afbd.apb.all;
+  library lapb;
+    use lapb.apb.all;
 
-   library general_cores;
-      use general_cores.wishbone_pkg.all;
+  library afbd;
+    use afbd.apb;
+    use afbd.apb.all;
 
-   library ltypes;
-      use ltypes.types.all;
+  library ltypes;
+    use ltypes.types.all;
 
-   library uvvm_util;
-      context uvvm_util.uvvm_util_context;
-
-   library bitvis_vip_wishbone;
-      use bitvis_vip_wishbone.wishbone_bfm_pkg.all;
 end context;
