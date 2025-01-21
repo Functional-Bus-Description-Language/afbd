5f7e015dd5ce5b8d519566d3dbc0c8b3a3c85422
Michał Kruszewski <mkru@protonmail.com>
2025-01-21 21:12:32 +0100

    Bring back cons-1 tb

---
+++ tests/co-simulations/const/1/vhdl-apb/tb.vhd
@@ -0,0 +1,46 @@
+library afbd;
+  use afbd.main_pkg;
+
+library work;
+  context work.cosim_context;
+  use work.cosim.all;
+
+
+entity tb_cosim is
+  generic(
+    G_SW_GW_FIFO_PATH : string;
+    G_GW_SW_FIFO_PATH : string
+  );
+end entity;
+
+
+architecture test of tb_cosim is
+
+  signal clk : std_logic := '0';
+
+  signal st : std_logic_vector(to_integer(main_pkg.C) - 1 downto 0) := std_logic_vector(to_unsigned(to_integer(main_pkg.C), to_integer(main_pkg.C)));
+  signal stl : slv_vector(1 downto 0)(7 downto 0) := (
+    0 => std_logic_vector(to_unsigned(to_integer(main_pkg.CL(0)), 8)),
+    1 => std_logic_vector(to_unsigned(to_integer(main_pkg.CL(1)), 8))
+  );
+
+  signal req : requester_out_t;
+  signal com : completer_out_t;
+
+begin
+
+  clk <= not clk after 0.5 ns;
+
+  cosim_interface(G_SW_GW_FIFO_PATH, G_GW_SW_FIFO_PATH, clk, req, com);
+
+  afbd_main : entity afbd.Main
+  port map (
+    clk_i => clk,
+    rst_i => '0',
+    reqs_i(0) => req,
+    reqs_o(0) => com,
+    st_i => st,
+    stl_i => stl
+  );
+
+end architecture;
